eac235351f5d6902f5033e9d1919a694efce05d3
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-06-20 10:22:28 -0400

    more

--- src/raii.cpp
+++ src/raii.cpp
@@ -5,4 +5,7 @@ namespace autocgrouper {
 User::User(Settings &settings): settings(settings) {}
 AutoCGrouper::AutoCGrouper(Settings &settings): settings(settings) {}
 
+User::~User() {}
+AutoCGrouper::~AutoCGrouper() {}
+
 }
--- src/raii.h
+++ src/raii.h
@@ -6,22 +6,27 @@
 namespace autocgrouper{
 
 class CGroup {
+    int64_t pid;
+    std::string name;
 };
 
 class User {
     Settings &settings;
     std::string name;
-    std::unordered_map<std::string, CGroup> cgroups;
+    std::unordered_map<int64_t, CGroup> cgroups;
 public:
     User(Settings &settings);
+    ~User();
+    std::string setCGroup(int64_t pid, std::string);
 };
 
 class AutoCGrouper {
     Settings &settings;
+    std::unordered_map<std::string, User> users;
 public:
     AutoCGrouper(Settings &settings);
-
-    std::unordered_map<int64_t, User> users;
+    User &operator[](std::string username);
+    ~AutoCGrouper();
 };
 
 }
