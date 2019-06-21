77b7c41b114b5ce1f8245e3afa921b5881f07dda
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-06-21 00:51:21 -0400

    fix

--- azure/prepare.sh
+++ azure/prepare.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" >> /etc/apt/source.list
+sudo bash -c 'echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" >> /etc/apt/source.list'
 sudo apt-get update
+
+apt-cache search clang-tidy
+
 sudo apt-get install gcc-9 g++-9 clang-tidy clang-format
 sudo apt-get remove gcc-5 g++-5
