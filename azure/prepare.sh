0fdcf3635b2f8f7335cd92594f317a78d2dfd517
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-06-21 01:02:14 -0400

    try

--- azure/prepare.sh
+++ azure/prepare.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-sudo bash -c 'echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" >> /etc/apt/source.list'
+echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" | sudo tee /etc/apt/source.list
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-get update
 
-sudo apt-get install gcc-9 g++-9 clang-tidy-6.0 clang-format-6.0
+sudo apt-get install gcc-9 g++-9
 sudo apt-get remove gcc-5 g++-5
+
+sudo apt-get install clang-8 lldb-8 lld-8 clang-format-8 clang-tidy-8
 
 which clang-format
 which clang-tidy
