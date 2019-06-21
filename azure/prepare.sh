d19a2646d2cd9aaf6717d47c1f4f916bfc7dc9db
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-06-21 01:22:50 -0400

    link

--- azure/prepare.sh
+++ azure/prepare.sh
@@ -7,4 +7,6 @@ sudo apt-get update
 sudo apt-get install gcc-9 g++-9
 sudo apt-get remove gcc-5 g++-5
 
-sudo apt-get install clang-8 clang-format-8 clang-tidy-8 clang-format clang-tidy
+sudo apt-get install clang-8 clang-format-8 clang-tidy-8
+sudo ln -s /usr/bin/clang-format-8 /usr/bin/clang-format
+sudo ln -s /usr/bin/clang-tidy-8 /usr/bin/clang-tidy
