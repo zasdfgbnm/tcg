66d48b252b7402ec709aa205ca23e80d4bc21aa7
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-12-28 11:25:14 -0800

    save

--- src/self.cpp
+++ src/self.cpp
@@ -1,21 +1,28 @@
 #include <string>
 #include <unistd.h>
-#include <iostream>
+#include <fstream>
 
 #include <spdlog/spdlog.h>
 #include <fmt/core.h>
 
-#include "utils.h"
+#include "utils.hpp"
 
 void self() {
+  auto logger = spdlog::get("self");
+  logger->info("Getting the cgroup for the current shell");
   auto pid = getpid();
   auto cg = fmt::format("/proc/{}/cgroup", pid);
-  auto ud = user_dir();
+  logger->debug("Reading {}...", cg);
+  auto uid = getuid();
+  auto userdir = fmt::format("/terminals/{}/", uid);
   std::string line;
-  ifstream in(cg);
+  std::ifstream in(cg);
   while (std::getline(in, line)) {
-    if (line.find(ud) != std::string::npos) {
-      fmt::print("found")!
+    logger->debug("Parsing line: {}", line);
+    std::size_t pos;
+    if ((pos = line.find(userdir)) != std::string::npos) {
+      fmt::print(line.substr(pos + userdir.size()) + "\n");
+      break;
     }
   }
 }
