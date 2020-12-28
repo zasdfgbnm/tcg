ce7d57db4d1b773e7b1360e10c6516fe77f46f56
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-12-28 01:03:33 -0800

    Move builtin names to a txt file (#43)
    * save
    
    * save
    
    * save
    
    * Update CMakeLists.txt
    
    * Create .clang-tidy-ignore
    
    * Update .clang-tidy-ignore
    
    * Update .clang-tidy-ignore
    
    * Update and rename .clang-tidy-ignore to .clang-tidy
    
    * Update .clang-tidy
    
    * save

--- src/names.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <unordered_set>
-
-std::unordered_set<std::string> names = {
-    "newton",   "einstein", "schrodinger", "feynman", "fermi",      "noether",
-    "dirac",    "kepler",   "ampere",      "pauli",   "heisenberg", "hilbert",
-    "poincare", "maxwell",  "boltzmann",   "gibbs",   "hawking",    "lorentz",
-    "kelvin",   "klein",    "higgs",       "euler",   "gauss",      "euclid",
-    "faraday",  "riemann"};
