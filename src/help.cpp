0b01539ed5ec4c2957f1ebe74c6959873f5a0100
Xiang <qasdfgtyuiop@gmail.com>
2021-01-31 17:53:08 -0800

    save

--- src/help.cpp
+++ src/help.cpp
@@ -3,9 +3,14 @@
 #include <fmt/color.h>
 #include <fmt/core.h>
 
+const char *help_description = "display help information";
+
 void help() {
   fmt::print("Usage:\n");
-  fmt::print("tcg command [args...]\n");
+  fmt::print("tcg command [args...]\n\n");
+  fmt::print("Available commands:\n");
+  fmt::print(fg(fmt::color::red) | fmt::emphasis::bold, "help: ");
+  fmt::print(help_description);
 }
 
 void invalid_argument() {
