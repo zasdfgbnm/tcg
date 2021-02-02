5ce580f769fad314bb8932020bd7c2af7f2e99d5
Gao, Xiang <qasdfgtyuiop@gmail.com>
2021-02-02 08:33:43 -0800

    Migrate freezer to modern handler (#89)
    * Migrate freezer to modern handler
    
    * save
    
    * fix

--- src/freeze.cpp
+++ src/freeze.cpp
@@ -6,38 +6,48 @@
 #include "command.hpp"
 #include "utils.hpp"
 
-void freeze(const std::string &name) {
-  auto logger = spdlog::get("freeze");
-  logger->info("Will freeze {}.", name);
-  auto dir = name_dir(name, true);
-  auto freeze_file = dir + "/cgroup.freeze";
-  logger->info("Writing 1 to {}...", freeze_file);
-  auto out = fmt::output_file(freeze_file);
-  out.print("1");
-  out.close();
-  logger->info("Done freezing {}.", name);
-}
-
-void unfreeze(const std::string &name) {
-  auto logger = spdlog::get("freeze");
-  logger->info("Will unfreeze {}.", name);
-  auto dir = name_dir(name, true);
-  auto freeze_file = dir + "/cgroup.freeze";
-  logger->info("Writing 0 to {}...", freeze_file);
-  auto out = fmt::output_file(freeze_file);
-  out.print("0");
-  out.close();
-  logger->info("Done unfreezing {}.", name);
-}
-
 static Command command_f(/*name =*/"freeze",
                          /*alias =*/{"f"},
                          /*short_description =*/"TODO: Add doc",
                          /*long_description =*/R"body(TODO: Add doc)body",
-                         /*handlers =*/{freeze});
+                         /*handlers =*/{});
+
+static struct FreezeHandler final : public Handler {
+  FreezeHandler(Command &command) : Handler(command, {"name"_var}) {}
+  void operator()(
+      const std::unordered_map<std::string, std::string> &args) const override {
+    auto logger = spdlog::get("freeze");
+    std::string name = args.at("name");
+    logger->info("Will freeze {}.", name);
+    auto dir = name_dir(name, true);
+    auto freeze_file = dir + "/cgroup.freeze";
+    logger->info("Writing 1 to {}...", freeze_file);
+    auto out = fmt::output_file(freeze_file);
+    out.print("1");
+    out.close();
+    logger->info("Done freezing {}.", name);
+  }
+} freeze_handler(command_f);
 
 static Command command_uf(/*name =*/"unfreeze",
                           /*alias =*/{"uf"},
                           /*short_description =*/"TODO: Add doc",
                           /*long_description =*/R"body(TODO: Add doc)body",
-                          /*handlers =*/{unfreeze});
+                          /*handlers =*/{});
+
+static struct UnfreezeHandler final : public Handler {
+  UnfreezeHandler(Command &command) : Handler(command, {"name"_var}) {}
+  void operator()(
+      const std::unordered_map<std::string, std::string> &args) const override {
+    auto logger = spdlog::get("freeze");
+    std::string name = args.at("name");
+    logger->info("Will unfreeze {}.", name);
+    auto dir = name_dir(name, true);
+    auto freeze_file = dir + "/cgroup.freeze";
+    logger->info("Writing 0 to {}...", freeze_file);
+    auto out = fmt::output_file(freeze_file);
+    out.print("0");
+    out.close();
+    logger->info("Done unfreezing {}.", name);
+  }
+} unfreeze_handler(command_uf);
