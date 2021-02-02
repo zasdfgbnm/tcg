ff88c282a3353d8b6c7c28652ce92c0bc78fe05f
Xiang <qasdfgtyuiop@gmail.com>
2021-02-02 09:52:45 -0800

    save

--- src/self.cpp
+++ src/self.cpp
@@ -18,30 +18,26 @@ static Command command(/*name =*/"self",
                        /*sandbox =*/false // disable sandbox to access /proc
 );
 
-static struct SelfHandler final : public Handler {
-  SelfHandler(Command &command) : Handler(command, {}) {}
-  void operator()(
-      const std::unordered_map<std::string, std::string> &args) const override {
-    auto logger = spdlog::get("self");
-    logger->info("Getting the cgroup for the current shell");
-    auto pid = getpid();
-    auto cg = fmt::format("/proc/{}/cgroup", pid);
-    if (!fs::exists(fs::path(cg))) {
-      logger->info("Procfs not mounted, or cgroup not enabled");
-      exit(EXIT_FAILURE);
-    }
-    logger->debug("Reading {}...", cg);
-    auto uid = getuid();
-    auto userdir = fmt::format("/terminals/{}/", uid);
-    std::string line;
-    std::ifstream in(cg);
-    while (std::getline(in, line)) {
-      logger->debug("Parsing line: {}", line);
-      std::size_t pos;
-      if ((pos = line.find(userdir)) != std::string::npos) {
-        fmt::print(line.substr(pos + userdir.size()) + "\n");
-        break;
-      }
+DEFINE_HANDLER(command, {}, {
+  auto logger = spdlog::get("self");
+  logger->info("Getting the cgroup for the current shell");
+  auto pid = getpid();
+  auto cg = fmt::format("/proc/{}/cgroup", pid);
+  if (!fs::exists(fs::path(cg))) {
+    logger->info("Procfs not mounted, or cgroup not enabled");
+    exit(EXIT_FAILURE);
+  }
+  logger->debug("Reading {}...", cg);
+  auto uid = getuid();
+  auto userdir = fmt::format("/terminals/{}/", uid);
+  std::string line;
+  std::ifstream in(cg);
+  while (std::getline(in, line)) {
+    logger->debug("Parsing line: {}", line);
+    std::size_t pos;
+    if ((pos = line.find(userdir)) != std::string::npos) {
+      fmt::print(line.substr(pos + userdir.size()) + "\n");
+      break;
     }
   }
-} handler(command);
+});
