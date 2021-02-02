1d90105552e9a28dd6c33206d66ece618400cf77
Xiang <qasdfgtyuiop@gmail.com>
2021-02-02 07:35:51 -0800

    Migrate self to modern handler

--- src/self.cpp
+++ src/self.cpp
@@ -11,30 +11,6 @@
 
 namespace fs = boost::filesystem;
 
-void self() {
-  auto logger = spdlog::get("self");
-  logger->info("Getting the cgroup for the current shell");
-  auto pid = getpid();
-  auto cg = fmt::format("/proc/{}/cgroup", pid);
-  if (!fs::exists(fs::path(cg))) {
-    logger->info("Procfs not mounted, or cgroup not enabled");
-    exit(EXIT_FAILURE);
-  }
-  logger->debug("Reading {}...", cg);
-  auto uid = getuid();
-  auto userdir = fmt::format("/terminals/{}/", uid);
-  std::string line;
-  std::ifstream in(cg);
-  while (std::getline(in, line)) {
-    logger->debug("Parsing line: {}", line);
-    std::size_t pos;
-    if ((pos = line.find(userdir)) != std::string::npos) {
-      fmt::print(line.substr(pos + userdir.size()) + "\n");
-      break;
-    }
-  }
-}
-
 static Command command(/*name =*/"self",
                        /*alias =*/{"sf"},
                        /*short_description =*/"TODO: Add doc",
@@ -42,3 +18,31 @@ static Command command(/*name =*/"self",
                        /*handlers =*/{self},
                        /*sandbox =*/false // disable sandbox to access /proc
 );
+
+static struct SelfHandler final : public Handler {
+  SelfHandler(Command &command) : Handler(command, {}) {}
+  void operator()(
+      const std::unordered_map<std::string, std::string> &args) const override {
+    auto logger = spdlog::get("self");
+    logger->info("Getting the cgroup for the current shell");
+    auto pid = getpid();
+    auto cg = fmt::format("/proc/{}/cgroup", pid);
+    if (!fs::exists(fs::path(cg))) {
+      logger->info("Procfs not mounted, or cgroup not enabled");
+      exit(EXIT_FAILURE);
+    }
+    logger->debug("Reading {}...", cg);
+    auto uid = getuid();
+    auto userdir = fmt::format("/terminals/{}/", uid);
+    std::string line;
+    std::ifstream in(cg);
+    while (std::getline(in, line)) {
+      logger->debug("Parsing line: {}", line);
+      std::size_t pos;
+      if ((pos = line.find(userdir)) != std::string::npos) {
+        fmt::print(line.substr(pos + userdir.size()) + "\n");
+        break;
+      }
+    }
+  }
+} handler(command);
