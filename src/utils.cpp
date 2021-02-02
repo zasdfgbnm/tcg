30050942a1584b4e450d26a870cfecf00b62e358
Xiang <qasdfgtyuiop@gmail.com>
2021-02-01 17:44:06 -0800

    Use operator() to replace call

--- src/utils.cpp
+++ src/utils.cpp
@@ -53,7 +53,7 @@ std::map<std::string, const Command *> Command::registry;
 
 void invalid_argument();
 
-void handler::call(const char *args[]) const {
+void handler::operator()(const char *args[]) const {
   assert(args[num_arg_] == nullptr);
   switch (num_arg_) {
   case 0:
@@ -107,13 +107,13 @@ const Command *Command::get(const std::string &name) {
   return registry[name];
 }
 
-void Command::call(const char *args[]) const {
+void Command::operator()(const char *args[]) const {
   uint8_t num_arg = 0;
   while (args[num_arg] != nullptr)
     num_arg++;
   for (auto &h : handlers) {
     if (h.num_arg() == num_arg) {
-      h.call(args);
+      h(args);
       return;
     }
   }
