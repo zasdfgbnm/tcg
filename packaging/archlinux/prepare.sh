edb0791fc66ce1d5f15ac547e32b80d3ca083536
Gao, Xiang <qasdfgtyuiop@gmail.com>
2021-03-26 07:42:22 -0700

    Add non-git AUR package (#124)
    * Add non-git AUR package
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * fix
    
    * fix
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save
    
    * save

--- packaging/archlinux/prepare.sh
+++ packaging/archlinux/prepare.sh
@@ -3,16 +3,31 @@
 set -eux
 
 run () {
+    set -eux
     ver=$(python ../../../shells/xonsh/setup.py --version)
+    commit=$(git rev-parse HEAD)
     echo $ver
     sed -i "s/pkgver=x/pkgver=$ver/g" PKGBUILD
+    sed -i "s/commit=x/commit=$commit/g" PKGBUILD
     cat PKGBUILD
-    docker run -v $PWD:/w -w /tmp zasdfgbnm/archlinux-yaourt bash -c '
+    sudo docker run --privileged -v $PWD:/w -w /tmp zasdfgbnm/archlinux-yaourt bash -c '
+        set -eux
         cp -r /w .
         cd w
-        makepkg --printsrcinfo
-    ' | tee .SRCINFO
+        integ=$(makepkg -g)
+        sed -i "s/sha256sums=(x)/$integ/g" PKGBUILD
+        makepkg --printsrcinfo > .SRCINFO
+        sudo cp PKGBUILD .SRCINFO /w
+    '
+    sudo chown -R $(id -u):$(id -g) .
+    cat PKGBUILD
+    cat .SRCINFO
 }
 
-cd tcg-git
+pushd tcg
 run
+popd
+
+pushd tcg-git
+run
+popd
---
+++ packaging/archlinux/push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eux
+
+push_repo () {
+    git clone ssh://aur@aur.archlinux.org/$1.git
+    pushd $1
+    rm -rf ./*
+    cp ../packaging/archlinux/$1/{PKGBUILD,.SRCINFO} .
+    git add .
+    git commit -m "version bump"
+    git push
+    popd
+}
+
+push_repo tcg
+push_repo tcg-git
