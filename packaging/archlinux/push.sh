d7103e74af66de084d7808704efff8ffa69dda04
Gao, Xiang <qasdfgtyuiop@gmail.com>
2021-03-26 08:58:00 -0700

    Fixes AUR push (#125)
    * save
    
    * save

--- packaging/archlinux/push.sh
+++ packaging/archlinux/push.sh
@@ -3,14 +3,15 @@
 set -eux
 
 push_repo () {
-    git clone ssh://aur@aur.archlinux.org/$1.git
-    pushd $1
+    git clone ssh://aur@aur.archlinux.org/$1.git publish
+    pushd publish
     rm -rf ./*
     cp ../packaging/archlinux/$1/{PKGBUILD,.SRCINFO} .
     git add .
     git commit -m "version bump"
     git push
     popd
+    rm -rf publish
 }
 
 push_repo tcg
