a93055279d6bfae8be231667288f277d4ec150e2
Xiang <qasdfgtyuiop@gmail.com>
2021-03-26 06:44:43 -0700

    Add non-git AUR package

--- packaging/archlinux/prepare.sh
+++ packaging/archlinux/prepare.sh
@@ -4,15 +4,25 @@ set -eux
 
 run () {
     ver=$(python ../../../shells/xonsh/setup.py --version)
+    commit=$(git rev-parse HEAD)
     echo $ver
     sed -i "s/pkgver=x/pkgver=$ver/g" PKGBUILD
+    sed -i "s/commit=x/pkgver=$commit/g" PKGBUILD
     cat PKGBUILD
     docker run -v $PWD:/w -w /tmp zasdfgbnm/archlinux-yaourt bash -c '
-        cp -r /w .
-        cd w
-        makepkg --printsrcinfo
-    ' | tee .SRCINFO
+        cd /w
+        makepkg -g
+        makepkg --printsrcinfo > .SRCINFO
+    '
+    sudo chown -R $(id -u):$(id -g) .
+    cat PKGBUILD
+    cat .SRCINFO
 }
 
-cd tcg-git
+pushd tcg
 run
+popd
+
+pushd tcg-git
+run
+popd
