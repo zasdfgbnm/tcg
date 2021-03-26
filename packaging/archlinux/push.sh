d8e4ccdde0da13a573e55fe0855002c3b02f3cbd
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-03-26 07:44:59 -0700

    save

--- packaging/archlinux/push.sh
+++ packaging/archlinux/push.sh
@@ -3,8 +3,8 @@
 set -eux
 
 push_repo () {
-    git clone ssh://aur@aur.archlinux.org/$1.git
-    pushd $1
+    git clone ssh://aur@aur.archlinux.org/$1.git publish
+    pushd publish
     rm -rf ./*
     cp ../packaging/archlinux/$1/{PKGBUILD,.SRCINFO} .
     git add .
