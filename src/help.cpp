75861ae37f8c90e1bf4e237be07d03979b167a40
Gao, Xiang <qasdfgtyuiop@gmail.com>
2021-03-27 13:41:19 -0700

    Add version info to cpp (#135)
    * Add version info to cpp
    
    * save
    
    * save
    
    * save
    
    * format
    
    * save

--- src/help.cpp
+++ src/help.cpp
@@ -7,6 +7,7 @@
 #include <boost/assert.hpp>
 
 #include "command.hpp"
+#include "config.h"
 #include "utils.hpp"
 
 std::unordered_set<std::string> suggest_commands(std::string prefix);
@@ -29,7 +30,18 @@ Command command(/*name =*/"help",
                                    // docs on systems without cgroup v2
 );
 
+void print_version() {
+#ifdef VERSION
+  fmt::print(". version {}", VERSION);
+#endif
+}
+
 DEFINE_HANDLER({}, "show the help information for the entire tcg tool", {
+  // version
+  fmt::print("tcg: terminal cgroups");
+  print_version();
+  fmt::print("\n\n");
+
   // title
   fmt::print(title_format, "Usage:\n");
   fmt::print("tcg <command> [<args>]\n\n");
