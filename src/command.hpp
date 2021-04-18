df33abc77f388bf05404d24bae798c111f8160cc
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-04-17 19:11:06 -0700

    save

--- src/command.hpp
+++ src/command.hpp
@@ -142,7 +142,7 @@ public:
 
   Command(const std::string &name, const std::vector<std::string> &alias,
           const std::string &short_description,
-          const std::string &additional_note, Flags flags = {true, true});
+          const std::string &additional_note, Flags flags = {true, true, true, true});
 
   virtual bool defined() const { return true; }
 
